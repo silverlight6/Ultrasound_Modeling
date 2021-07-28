7d24febf500c7bdb147a41fda36b2f91c5a4b878
Silver Lucoris <slucoris@gmail.com>
2021-07-28 10:04:32 -0700

    numpy stuff

---
+++ Dataset_2.py
@@ -0,0 +1,127 @@
+import tensorflow as tf
+import numpy as np
+from DataAugs import dataAug
+
+
+def label2vec(label):
+    class_2 = np.where(label >= 1.05, label - 1, 0)
+    class_2 = np.where(class_2 > 1, 1, class_2)
+    class_1 = np.expand_dims(np.where(label > 0.95, 1 - class_2, 0), axis=3)
+    class_0 = np.expand_dims(np.where(label <= 0.95, 1, 0), axis=3)
+    class_2 = np.expand_dims(class_2, axis=3)
+    label = np.concatenate((class_0, class_1, class_2), axis=3)
+    return label
+
+
+class Dataset(object):
+
+    def __init__(self, train_path=None, val_path=None):
+
+        print("\nInitializing Dataset...")
+        train_data = np.load(train_path, allow_pickle=True)
+        val_data = np.load(val_path, allow_pickle=True)
+
+        # The first 0 is due to how the .append works in the playground file
+        # The second 0 is because the label is in the first layer of the data.
+        y_tr = train_data[:, 0, :, :, 0]
+        y_te = val_data[:, 0, :, :, 0]
+        train_data = np.delete(train_data, 0, 4)
+        val_data = np.delete(val_data, 0, 4)
+        x_tr = np.array(train_data)
+        x_te = np.array(val_data)
+        # The -1 here is because the last layer is the bMode and I am not using the bMode in the training data
+        # This is simply my choice, feel free to change that but be aware that the number of input layers
+        # Moves from 10 to 11 and that will affect some lines of code in the evaluator file.
+        x_tr = x_tr[:, 0, :, :, :-1]
+        x_te = x_te[:, 0, :, :, :-1]
+        # This is float64 by default but needs to be float 32 for np.where function.
+        y_tr = y_tr.astype(dtype=np.float32)
+        y_te = y_te.astype(dtype=np.float32)
+
+        self.x_tr, self.y_tr = x_tr, y_tr
+        self.x_te, self.y_te = x_te, y_te
+
+        self.num_tr, self.num_te = self.x_tr.shape[0], self.x_te.shape[0]
+        self.idx_tr, self.idx_te = 0, 0
+
+        self.y_tr = y_tr
+        self.y_te = y_te
+
+        self.num_tr, self.num_te = self.x_tr.shape[0], self.x_te.shape[0]
+        self.idx_tr, self.idx_te = 0, 0
+
+        print("Number of data\nTraining: %d, Test: %d\n" % (self.num_tr, self.num_te))
+        print("x_tr shape = {}".format(x_tr.shape))
+        print("y_tr shape = {}".format(y_tr.shape))
+
+        x_sample, y_sample = self.x_te[0], self.y_te[0]
+        self.height = x_sample.shape[0]
+        self.width = x_sample.shape[1]
+        self.channel = x_sample.shape[2]
+
+        self.min_val, self.max_val = x_sample.min(), x_sample.max()
+        # self.num_class = int(np.floor(y_te.max()+1))
+        self.num_class = 3
+
+        print("Information of data")
+        print("Shape  Height: %d, Width: %d, Channel: %d" % (self.height, self.width, self.channel))
+        print("Value  Min: %.3f, Max: %.3f" % (self.min_val, self.max_val))
+        print("Class  %d" % self.num_class)
+
+    # This is converting the labels from 1d to 3d probability maps
+    # 1.05 because resize can sometimes change values up and down by about .01 at any given pixel
+    # if > 1 = 1 is because resize sometimes makes values at 101% up to 105%. Putting a cap to ensure
+    # this behavior is stopped.
+    # The classes don't need to add up to 100. It is simply a nice thing if they do.
+    # You only really need each pixel of importance to have a contribution to the loss.
+
+    def reset_idx(self): self.idx_tr, self.idx_te = 0, 0
+
+    # Get the next batch of training data
+    def next_train(self, batch_size=1, fix=False):
+
+        start, end = self.idx_tr, self.idx_tr+batch_size
+        x_tr, y_tr = np.copy(self.x_tr[start:end]), np.copy(self.y_tr[start:end])
+
+        terminator = False
+        if end >= self.num_tr:
+            terminator = True
+            self.idx_tr = 0
+            # self.x_tr, self.y_tr = shuffle(self.x_tr, self.y_tr)
+        else:
+            self.idx_tr = end
+
+        if fix:
+            self.idx_tr = start
+
+        if x_tr.shape[0] != batch_size:
+            x_tr, y_tr = np.copy(self.x_tr[-1-batch_size:-1]), np.copy(self.y_tr[-1-batch_size:-1])
+
+        # data aug goes here.
+        trShape = x_tr.shape
+        for i in range(0, trShape[0]):
+            x_tr[i, :, :, :], y_tr[i, :, :] = dataAug(x_tr[i, :, :, :], y_tr[i, :, :])
+
+        y_tr = label2vec(y_tr)
+        y_tr = tf.convert_to_tensor(y_tr, dtype=tf.float32)
+        return x_tr, y_tr, terminator
+
+    # Get the next batch of test data. This is always 1 for batch size in this model up to this point
+    def next_test(self, batch_size=1):
+
+        start, end = self.idx_te, self.idx_te + batch_size
+        x_te, y_te = self.x_te[start:end], self.y_te[start:end]
+
+        terminator = False
+        if end >= self.num_te:
+            terminator = True
+            self.idx_te = 0
+        else:
+            self.idx_te = end
+
+        if x_te.shape[0] != batch_size:
+            x_te, y_te = self.x_te[-1-batch_size:-1], self.y_te[-1-batch_size:-1]
+
+        y_te = label2vec(y_te)
+        y_te = tf.convert_to_tensor(y_te, dtype=tf.float32)
+        return x_te, y_te, terminator
---
+++ MainNumpy.py
@@ -0,0 +1,166 @@
+import tensorflow as tf
+import datetime
+import os
+from Dataset_2 import Dataset
+from VisionTransformer import VisionTransformer
+
+os.environ["CUDA_VISIBLE_DEVICES"] = "1"
+wDecay = None
+
+
+class Process(object):
+    def __init__(self, batch_size=16):
+        self.summary_writer = tf.summary.create_file_writer(
+            "logs/fit/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S"))
+        self.batch_size = batch_size
+        self.input_shape = [256, 80, 10]
+        self.precision = tf.keras.metrics.Precision(name='precision')
+        self.recall = tf.keras.metrics.Recall(name='recall')
+        self.pre_c2 = tf.keras.metrics.Precision(name='precision_c2')
+        self.re_c2 = tf.keras.metrics.Recall(name='recall_c2')
+        self.mio = tf.keras.metrics.MeanIoU(name='mean_iou', num_classes=3)
+        self.tr_recall = tf.keras.metrics.Recall(name='tr_recall')
+        self.tr_precision = tf.keras.metrics.Precision(name='tr_precision')
+        self.tr_mio = tf.keras.metrics.MeanIoU(name='tr_mio', num_classes=3)
+
+        self.test_iter = 0
+
+    def training(self, neuralnet, dataset, epochs):
+
+        print("\nTraining to %d epochs (%d of minibatch size)" % (epochs, self.batch_size))
+
+        global wDecay
+        iteration = 0
+        prev_loss = 0
+        # tf.keras.utils.plot_model(neuralnet.resModel, to_file='ResNeSt.png', show_shapes=True)
+
+        for epoch in range(epochs):
+            while True:
+                # Get the data for the next batch
+                x_tr, y_tr, terminator = dataset.next_train(self.batch_size)  # y_tr does not used in this prj.
+                # Take a step from that batch
+                loss, class_score = neuralnet.train_step(x=x_tr, y=y_tr)
+                # Loss is 256x80 so reduce to 1 number
+                loss = tf.reduce_sum(loss)
+                iteration += 1
+                class_score_M = tf.math.round(class_score)
+                y_tr_M = tf.math.round(y_tr)
+                self.tr_recall.update_state(y_tr_M, class_score_M)
+                self.tr_precision.update_state(y_tr_M, class_score_M)
+                self.tr_mio.update_state(y_tr_M, class_score_M)
+                if iteration % 491 == 0:
+                    with self.summary_writer.as_default():
+                        # tf.summary.image("Train Logits" + str(iteration), class_score, max_outputs=5, step=iteration)
+                        # tf.summary.image("Train Target" + str(iteration), y_tr, max_outputs=5, step=iteration)
+                        tf.summary.image("Train" + str(iteration),
+                                         tf.concat([class_score, y_tr], axis=2),
+                                         max_outputs=5, step=iteration)
+
+                print('.', end='')
+                if (iteration + 1) % 100 == 0:
+                    print()
+                if terminator:
+                    break
+
+                # neuralnet.save_params()
+            print()
+            print("Epoch [%d / %d] (%d iteration)  Loss:%.5f, Recall:%.5f, Precision:%.5f, IoU:%.5f"
+                  % (epoch, epochs, iteration, loss, self.tr_recall.result(), self.tr_precision.result(),
+                     self.tr_mio.result()))
+            self.tr_recall.reset_states()
+            self.tr_precision.reset_states()
+            self.tr_mio.reset_states()
+            if prev_loss == loss:
+                print("Model is throwing a fit")
+                print(class_score)
+            prev_loss = loss
+            if epoch % 5 == 0:
+                self.test(neuralnet, dataset, epoch)
+            # test(neuralnet, dataset, epoch)
+            schedule = tf.optimizers.schedules.PiecewiseConstantDecay(
+                [5000, 10000, 20000, 30000, 40000], [1e-0, 3e-1, 1e-1, 3e-2, 1e-2, 3e-3])
+            # lr and wd can be a function or a tensor
+            neuralnet.learning_rate = 1e-2 * schedule(iteration)
+            neuralnet.weight_decay = lambda: 1e-4 * schedule(iteration)
+            wDecay = lambda: 1e-4 * schedule(iteration)
+
+    def test(self, neuralnet, dataset, epoch):
+        print("\nTest...")
+        # Much of this code is copied from the ResNest source I found and then translated to Keras
+        # This simply calculates the metrics listed at the top using the logits and true
+        # I do not know if this is 100% bug free since switching to probability labels
+        total_loss = 0
+        while True:
+            x_te, y_te, terminator = dataset.next_test(16)  # y_te does not used in this prj.
+            loss, class_score = neuralnet.step(x=x_te, y=y_te)
+            loss = tf.reduce_sum(loss)
+            class_score_M = tf.math.round(class_score)
+            y_te_M = tf.math.round(y_te)
+            self.precision.update_state(y_te_M, class_score_M)
+            self.recall.update_state(y_te_M, class_score_M)
+            self.pre_c2.update_state(y_te_M[:, :, -1], class_score_M[:, :, -1])
+            self.re_c2.update_state(y_te_M[:, :, -1], class_score_M[:, :, -1])
+            self.mio.update_state(y_te_M, class_score_M)
+            total_loss += loss
+            if self.test_iter % 23 == 0:
+                with self.summary_writer.as_default():
+                    # tf.summary.image("Test Logits" + str(test_iter), class_score, step=test_iter)
+                    # tf.summary.image("Test Target" + str(test_iter), y_te, step=test_iter)
+                    tf.summary.image("Test" + str(self.test_iter), tf.concat([class_score, y_te], axis=2),
+                                     step=self.test_iter)
+            if terminator:
+                break
+            self.test_iter += 1
+
+        # This half of the code prints the metrics to the screen and saves them to a log file.
+        # Later, you can open them up on tensorboard to see the progress.
+        # total_loss /= dataset.num_te
+        f1 = 2 * (self.precision.result() * self.recall.result()) / (self.precision.result() + self.recall.result())
+        f1_2 = 2 * (self.pre_c2.result() * self.re_c2.result()) / (self.pre_c2.result() + self.re_c2.result())
+
+        with self.summary_writer.as_default():
+            tf.summary.scalar("loss", total_loss, step=epoch)
+            tf.summary.scalar("mean_IoU", self.mio.result(), step=epoch)
+            tf.summary.scalar("val_f1", f1, step=epoch)
+            tf.summary.scalar("val_precision", self.precision.result(), step=epoch)
+            tf.summary.scalar("recall_recall", self.recall.result(), step=epoch)
+            tf.summary.scalar("c2_f1", f1_2, step=epoch)
+            tf.summary.scalar("c2_precision", self.pre_c2.result(), step=epoch)
+            tf.summary.scalar("c2_recall", self.re_c2.result(), step=epoch)
+            tf.summary.scalar("loss", total_loss, step=epoch)
+
+        print("loss = {}".format(total_loss))
+        print("IoU = {}".format(self.mio.result()))
+        print("f1 = {}".format(f1))
+        print("precision = {}".format(self.precision.result()))
+        print("recall = {}".format(self.recall.result()))
+        print("learning rate = {}".format(neuralnet.learning_rate))
+
+        self.mio.reset_states()
+        self.precision.reset_states()
+        self.recall.reset_states()
+        self.pre_c2.reset_states()
+        self.re_c2.reset_states()
+        return f1
+
+
+def main():
+    # os.environ["CUDA_VISIBLE_DEVICES"] = "1"
+    train_data = '/home/silver/TBI/NPFiles/Disp/TrainingData.npy'
+    val_data = '/home/silver/TBI/NPFiles/Disp/TestingData.npy'
+    dataset = Dataset(train_data, val_data)
+    # config = tf.estimator.RunConfig(train_distribute=mirrored_strategy)
+    # with mirrored_strategy.scope():
+    neuralnet = VisionTransformer()
+    batch_size = 16
+    # tf.keras.utils.model_to_dot(neuralnet.visionModel, to_file='TransUNet_dot.png', show_shapes=True)
+
+    # print(neuralnet.visionModel.summary())
+    print(len(neuralnet.visionModel.layers))
+    process = Process(batch_size=batch_size)
+    process.training(neuralnet=neuralnet, dataset=dataset, epochs=51)
+    neuralnet.visionModel.save('/home/silver/TBI/Models/ResNeSt_T0')
+
+
+if __name__ == '__main__':
+    main()
