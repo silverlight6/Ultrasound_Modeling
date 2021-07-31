e8cd9076d92e19fb48430c5bb7933622c50d33f5
Silver Lucoris <slucoris@gmail.com>
2021-07-31 14:47:56 -0700

    KSAC SWIN and Playground

--- Decoder.py
+++ Decoder.py
@@ -1,4 +1,6 @@
 import tensorflow as tf
+import numpy as np
+from tensorflow.python.keras.utils import conv_utils
 
 
 # This may see some changes if I want to use a different style of decoder
@@ -27,10 +29,10 @@ class DecoderBlock(tf.Module):
                                               kernel_regularizer=self.wDecay)
         # self.upsample = tf.keras.layers.UpSampling2D()
         self.LeakyReLU1 = tf.keras.layers.LeakyReLU()
-        self.bn1_0 = tf.keras.layers.experimental.SyncBatchNormalization()
-        self.bn1_1 = tf.keras.layers.experimental.SyncBatchNormalization()
-        self.bn1_2 = tf.keras.layers.experimental.SyncBatchNormalization()
-        self.bn1_3 = tf.keras.layers.experimental.SyncBatchNormalization()
+        self.bn1_0 = tf.keras.layers.BatchNormalization()
+        self.bn1_1 = tf.keras.layers.BatchNormalization()
+        self.bn1_2 = tf.keras.layers.BatchNormalization()
+        self.bn1_3 = tf.keras.layers.BatchNormalization()
         self.conv2_0 = tf.keras.layers.Conv2D(out_channels // 4, 1, strides=1, padding='SAME',
                                               kernel_initializer=tf.keras.initializers.HeNormal(),
                                               kernel_regularizer=self.wDecay)
@@ -46,10 +48,10 @@ class DecoderBlock(tf.Module):
                                               kernel_initializer=tf.keras.initializers.HeNormal(),
                                               kernel_regularizer=self.wDecay,
                                               dilation_rate=(8, 8))
-        self.bn2_0 = tf.keras.layers.experimental.SyncBatchNormalization()
-        self.bn2_1 = tf.keras.layers.experimental.SyncBatchNormalization()
-        self.bn2_2 = tf.keras.layers.experimental.SyncBatchNormalization()
-        self.bn2_3 = tf.keras.layers.experimental.SyncBatchNormalization()
+        self.bn2_0 = tf.keras.layers.BatchNormalization()
+        self.bn2_1 = tf.keras.layers.BatchNormalization()
+        self.bn2_2 = tf.keras.layers.BatchNormalization()
+        self.bn2_3 = tf.keras.layers.BatchNormalization()
 
         # self.up = tf.keras.layers.Conv2DTranspose(out_channels, kernel_size=4, strides=2, padding='same')
         self.up = tf.keras.layers.Conv2DTranspose(out_channels, 3, strides=2, padding='Same',
@@ -142,3 +144,203 @@ class DecoderCup(tf.Module):
 
     def __call__(self, hidden_states, features=None, *args, **kwargs):
         return self.forward(hidden_states, features)
+
+
+# This may see some changes if I want to use a different style of decoder
+class KSACBlock(tf.Module):
+    def __init__(self, out_channels, wDecay=None):
+        super().__init__()
+        self.wDecay = wDecay
+        self.kernel_size = [3, 3]
+        self.out_channels = out_channels
+        self.conv1 = KernelSharingConv(self.out_channels, self.kernel_size, kernel_regularizer=self.wDecay,
+                                       name='KSAC_1', kernel_initializer='HeNormal')
+        self.conv2 = KernelSharingConv(self.out_channels, self.kernel_size, kernel_regularizer=self.wDecay,
+                                       name='KSAC_1', kernel_initializer='HeNormal')
+        self.up = tf.keras.layers.Conv2DTranspose(out_channels, 3, strides=2, padding='Same', kernel_regularizer=wDecay,
+                                                  kernel_initializer=tf.keras.initializers.HeNormal())
+
+    def forward(self, x, skip=None):
+        # input comes in [256, 128, 64]
+        x = self.up(x)
+        if skip is not None:
+            # skips come in d = [256, 128, 64]
+            x = tf.concat([x, skip], axis=3)
+        x = self.conv1(x)
+        x = tf.convert_to_tensor(x)
+        x = self.conv2(x)
+        x = tf.convert_to_tensor(x)
+        return x
+
+    def __call__(self, x, skip=None, *args, **kwargs):
+        return self.forward(x, skip)
+
+
+@tf.function
+def compute_shift_and_paddings(k_height, k_width, dilation_v=1, dilation_h=1):
+    kernel_height_center = k_height // 2
+    kernel_width_center = k_width // 2
+
+    shifts_list = []
+    paddings_list = []
+
+    for i in range(0, k_height):
+        for j in range(0, k_width):
+            v_shift = (kernel_height_center - i) * dilation_v
+            h_shift = (kernel_width_center - j) * dilation_h
+
+            v_slice_start = -v_shift if v_shift < 0 else 0
+            h_slice_start = -h_shift if h_shift < 0 else 0
+
+            v_slice_end = -v_shift if v_shift > 0 else 0
+            h_slice_end = -h_shift if h_shift > 0 else 0
+
+            shifts = [v_slice_start, v_slice_end, h_slice_start, h_slice_end]
+            shifts_list += [shifts]
+
+            paddings = [[0, 0]]
+            paddings += [[v_shift, 0]] if v_shift > 0 else [[0, -v_shift]]
+            paddings += [[h_shift, 0]] if h_shift > 0 else [[0, -h_shift]]
+            # This is in the original code but like.. wtf..
+            paddings += [[0, 0]]
+
+            paddings_list += [paddings]
+
+    return shifts_list, paddings_list
+
+
+@tf.function
+def get_shift_and_paddings(k_height, k_width, dilations=None):
+    if dilations is None:
+        dilations = [1, 1]
+    elif isinstance(dilations, int):
+        dilations = [dilations, dilations]
+
+    dilations = list(dilations)
+
+    shifts_list, paddings_list = compute_shift_and_paddings(k_height, k_width, dilations[0], dilations[1])
+
+    return tf.convert_to_tensor(shifts_list), tf.convert_to_tensor(paddings_list)
+
+
+@tf.function
+def kernel_sharing_conv2d(inputs, filters, strides=(1, 1), dilation_rates_list=None, name=None):
+    if name is None:
+        name = "kernel_sharing_conv2d"
+
+    with tf.name_scope(name=name):
+
+        x = inputs  # [N, H, W, c]
+        k = filters  # [h, w, c, C]
+
+        inputs_shape = tf.shape(x)
+        batch_size = inputs_shape[0]
+        height = inputs_shape[1]
+        width = inputs_shape[2]
+        in_channels = x.shape[-1]
+
+        k_height, k_width, _, out_channels = k.shape
+
+        k_size = k_height * k_width
+
+        x = tf.raw_ops.Reshape(tensor=x, shape=[batch_size, height * width, in_channels])  # [N, HW, c]
+        k = tf.raw_ops.Reshape(tensor=k, shape=[k_size, 1, in_channels, out_channels])
+
+        y = tf.zeros(shape=[batch_size, height, width, out_channels], dtype=inputs.dtype)
+        y = y[:, ::strides[0], ::strides[1], :]
+
+        y_shape = tf.TensorShape(y.shape)
+
+        shifts_list_groups = []
+        paddings_list_groups = []
+        y_groups = []
+
+        for dilations in dilation_rates_list:
+            shifts_list, paddings_list = get_shift_and_paddings(k_height, k_width, dilations=dilations)
+
+            shifts_list_groups += [shifts_list]
+            paddings_list_groups += [paddings_list]
+
+            y_groups += [y]
+
+        for i in range(k_size):
+
+            value = tf.raw_ops.BatchMatMulV2(x=x, y=k[i])  # [N, HW, C]
+            value = tf.raw_ops.Reshape(tensor=value, shape=[batch_size, height, width, out_channels])
+
+            for j in range(len(dilation_rates_list)):
+                shifts_list = shifts_list_groups[j]
+                paddings_list = paddings_list_groups[j]
+
+                shifts = shifts_list[i]
+
+                v_bottom_shift = tf.raw_ops.SelectV2(condition=shifts[1] == 0, t=height, e=shifts[1])
+                h_right_shift = tf.raw_ops.SelectV2(condition=shifts[3] == 0, t=width, e=shifts[3])
+
+                value = value[:, shifts[0]: v_bottom_shift, shifts[2]: h_right_shift, :]  # [N, H - ?, W - ?, C]
+
+                paddings = paddings_list[i]
+
+                value = tf.raw_ops.Pad(input=value, paddings=paddings)
+                value = value[:, ::strides[0], ::strides[1], :]
+                value = tf.raw_ops.EnsureShape(input=value, shape=y_shape)
+
+                y_groups[j] = tf.raw_ops.Add(x=y_groups[j], y=value)
+
+        return y_groups
+
+
+# default was (1, 6, 12, 18, 24), switching to (1, 2, 4, 8, 16) for my purposes because of smaller input
+class KernelSharingConv(tf.keras.layers.Layer):
+    def __init__(self, filters, kernel_size, dilation_rates_list=(1, 2, 4, 8, 16), trainable=True,
+                 kernel_initializer='glorot_uniform', kernel_regularizer=None, use_bn=True, name=None):
+        super().__init__(trainable=trainable, name=name)
+
+        # Initialized for standard conv, if 3d conv, I believe this should be 3.
+        rank = 2
+
+        if isinstance(filters, float):
+            filters = int(filters)
+
+        self.filters = filters
+        self.kernel_size = conv_utils.normalize_tuple(kernel_size, rank, 'kernel_size')
+        self.dilation_rates_list = dilation_rates_list
+
+        self.kernel_initializer = tf.keras.initializers.get(kernel_initializer)
+        self.kernel_regularizer = tf.keras.regularizers.get(kernel_regularizer)
+
+        self.trainable = trainable
+
+        self.use_bn = use_bn
+
+    def build(self, input_shape):
+        input_shape = tf.TensorShape(input_shape)
+        input_channel = input_shape[-1]
+
+        kernel_shape = self.kernel_size + (input_channel, self.filters)
+
+        self.kernel = self.add_weight(name='kernel',
+                                      shape=kernel_shape,
+                                      initializer=self.kernel_initializer,
+                                      regularizer=self.kernel_regularizer,
+                                      trainable=True,
+                                      dtype=self.dtype)
+
+        if self.use_bn:
+            # self.bns = [tf.keras.layers.experimental.SyncBatchNormalization(name=f"bn_r_{r}")
+            #             for r in self.dilation_rates_list]
+            self.bns = [tf.keras.layers.BatchNormalization(name=f"bn_r_{r}") for r in self.dilation_rates_list]
+
+    def call(self, inputs, training=None):
+        y_list = kernel_sharing_conv2d(inputs, filters=self.kernel, dilation_rates_list=self.dilation_rates_list)
+
+        for i in range(len(y_list)):
+
+            x = y_list[i]
+
+            if self.use_bn:
+                x = self.bns[i](x, training=training)
+
+            # originally relu but I am switching this to gelu to keep it consistent with the rest of the model.
+            y_list[i] = tf.nn.gelu(x)
+        return y_list
---
+++ PizzaPlayGround.py
@@ -0,0 +1,182 @@
+import numpy as np
+import os                           # reading files
+import cv2                          # resize
+import math
+import multiprocessing
+import time
+import matplotlib
+from sklearn.utils import shuffle   # shuffle seed
+
+from matplotlib import pyplot as plt
+from scipy.io import loadmat
+
+matplotlib.use('Agg')
+
+
+def output2DImages(iteration):
+    # Make arrays for dividing data between training, testing, and validation.
+    manager = multiprocessing.Manager()
+
+    trainingData = manager.list()
+    testingData = manager.list()
+    trainingPaths = manager.list()
+    testingPaths = manager.list()
+
+    IPH_patients = [8, 9, 10, 12, 24, 47, 53, 62, 66, 67, 69, 74, 75, 78, 85, 89, 93,
+                    101, 105, 107, 110, 112, 113, 120, 121, 126, 129, 130, 133]
+    bad_patients = [1, 14, 22, 23, 27, 28, 32, 34, 35, 36, 37, 38, 39, 44, 49, 69, 71, 78, 82, 90, 98, 101, 121, 124,
+                    128, 133, 136, 928]
+    timeStart = np.zeros([100])
+    timeEnd = np.zeros([100])
+    # counting files
+    count = 0
+
+    def fileLoop(path, patient_num, iteration, mode):
+        iteration = iteration % 10
+        for file in os.listdir(path):
+            if ".mat" in file:
+                hPath = os.path.join(path, file)                  # put file name on path
+                pathName = file[0:17]                             # get path to print
+                Harmonics = loadmat(hPath)
+
+                bloodMask = np.array(list(Harmonics['bloodMaskThick']))
+                brainMask = np.array(list(Harmonics['brainMask']))
+                bMode = np.array(list(Harmonics['bModeNorm']))
+                # print("Keys are {}".format(Harmonics.keys()))
+
+                if mode == 0:
+                    harmonic = np.array(list(Harmonics['harmonics']))
+                    # separate real and imaginary.
+                    real = harmonic.real
+                    imag = harmonic.imag
+                else:
+                    displacement = np.array(list(Harmonics['displacement']))
+                    hrTimes = np.array(list(Harmonics['hrTimes']))
+                    hrshape = hrTimes.shape
+                    disshape = displacement.shape
+                    real = np.zeros([disshape[0], disshape[1], 5, hrshape[1] - 1])
+                    imag = np.zeros([disshape[0], disshape[1], 5, hrshape[1] - 1])
+                    for h in range(0, hrshape[1] - 1):
+                        start = int(math.ceil(30 * hrTimes[0, h]))
+                        real[:, :, :, h] = displacement[:, :, start:start + 5]
+                        imag[:, :, :, h] = displacement[:, :, start + 5:start + 10]
+                    real = np.array(real)
+                    imag = np.array(imag)
+
+                bMode = np.log10(bMode)
+
+                # Smooth & Create the Label
+                label = bloodMask + 1
+                label = label.astype('float32')
+                brainMask = cv2.resize(brainMask, (80, 256))
+                label = cv2.resize(label, (80, 256))
+                label = np.where(brainMask == 0, 0, label)
+                # # This code is for finding the mask
+                # label = np.where(brainMask == 0, 0, 1)
+                label = label.reshape([256, 80, 1])
+                cycles = real.shape[-1]
+                real = real.astype('float64')
+                imag = imag.astype('float64')
+                bMode = bMode.astype('float64')
+                bMode = np.mean(bMode, axis=2)
+
+                for k in range(0, cycles):
+                    realO = real[:, :, :, k]
+                    imagO = imag[:, :, :, k]
+                    bModeO = bMode[:, :, k]
+
+                    realO = realO - realO.mean(axis=0).mean(axis=0)
+                    safe_max = np.abs(realO).max(axis=0).max(axis=0)
+                    safe_max[safe_max == 0] = 1
+                    realO = realO / safe_max
+
+                    imagO = imagO - imagO.mean(axis=0).mean(axis=0)
+                    safe_max = np.abs(imagO).max(axis=0).max(axis=0)
+                    safe_max[safe_max == 0] = 1
+                    imagO = imagO / safe_max
+
+                    realO = cv2.resize(realO, (80, 256))
+                    imagO = cv2.resize(imagO, (80, 256))
+                    bModeO = cv2.resize(bModeO, (80, 256))
+
+                    # delete non-brain from input data
+                    for i in range(0, realO.shape[-1]):
+                        realO[:, :, i] = np.where(brainMask == 0, 0.0, realO[:, :, i])
+                        imagO[:, :, i] = np.where(brainMask == 0, 0.0, imagO[:, :, i])
+
+                    bModeO = bModeO.reshape([256, 80, 1])
+
+                    # concatenate the columns into one structure
+                    image = np.concatenate((label, realO, imagO, bModeO), axis=2)
+
+                    lock = multiprocessing.Lock()
+                    lock.acquire()
+                    if count % 10 == iteration:
+                        testingData.append([image])
+                        testingPaths.append([pathName])
+                    else:
+                        trainingData.append([image])
+                        trainingPaths.append([pathName])
+
+                    lock.release()
+                # for testing purposes only
+        timeEnd[count] = time.time()
+        print("Time for count {} = {} seconds".format(count, timeEnd[count] - timeStart[count]))
+        return
+
+    # data paths; it is just what it sounds like
+    # watch out for polar versus non-polar
+    dataPaths = "/home/silver/TBI/CardiacData/"
+
+    # make sure that data gets looked at even
+
+    pathlist = os.listdir(dataPaths)
+    pathlist = np.sort(pathlist)
+    pathlist = shuffle(pathlist, random_state=20)
+    t = 0
+    pLength = len(pathlist)
+
+    while count < pLength:
+        processes = []
+        while t < 10 and count < pLength:
+            fpath = os.path.join(dataPaths, pathlist[count])
+            patient_num = fpath[-3:]
+            patient_num = int(patient_num)
+            timeStart[count] = time.time()
+            if patient_num not in bad_patients:
+                p = multiprocessing.Process(target=fileLoop, args=(fpath, patient_num, iteration, 1))
+                p.start()
+                processes.append(p)
+            count += 1
+            t += 1
+        t = 0
+        for process in processes:
+            process.join()
+
+    # convert to numpy arrays, because the data is 4D
+    trainingData = np.array(trainingData)
+    testingData = np.array(testingData)
+    trainingPaths = np.array(trainingPaths)
+    testingPaths = np.array(testingPaths)
+
+    random_seed = np.random.randint(0, 1e5)
+    trainingData = shuffle(trainingData, random_state=random_seed)
+    testingData = shuffle(testingData, random_state=random_seed // 17)
+    trainingPaths = shuffle(trainingPaths, random_state=random_seed)
+    testingPaths = shuffle(testingPaths, random_state=random_seed // 17)
+
+    # let us see what we got
+    # print(testingPaths)
+    print("training {}".format(trainingData.shape))
+    print("testing {}".format(testingData.shape))
+
+    savePath = "/home/silver/TBI/NPFiles/Disp/"
+    print("saved in : {}".format(savePath))
+    np.save(savePath + "TrainingData.npy", trainingData)
+    np.save(savePath + "TestingData.npy", testingData)
+    np.save(savePath + "TrainingPaths.npy", trainingPaths)
+    np.save(savePath + "TestingPaths.npy", testingPaths)
+
+
+if __name__ == '__main__':
+    output2DImages(9)
