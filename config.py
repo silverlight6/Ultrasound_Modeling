873da8d5ee007e4fda128c09fc140f4e6d0d87b4
JPhan <jackphan92@hotmail.com>
2021-08-03 19:14:59 -0700

    Add comments to config file

--- config.py
+++ config.py
@@ -2,5 +2,9 @@ import os
 import dotenv
 
 dotenv.load_dotenv()
+
+# Used in DataFilePlayGround_2.py
+# Path to raw data
 RAW_DATA_PATH = str(os.getenv("RAW_DATA_PATH"))
+# Path to processed numpy matrices
 PROCESSED_NUMPY = str(os.getenv("PROCESSED_NUMPY"))
